a2a11a93e00f239747b5645ffbe0c923611ce1f5
David Castrillón <david.castri@hotmail.es>
2021-10-29 21:02:37 +0200

    Rutas hechas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { FormComponent } from './prova-tecnica/components/form/form.component';
+import { OutputComponent } from './prova-tecnica/components/output/output.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/form',
+    pathMatch: 'full'
+  },
+  {
+    path: 'form',
+    component: FormComponent    
+  },
+  {
+    path: 'output',
+    component: OutputComponent
+  },  
+  {
+    path: '**',
+    redirectTo: '/form'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
