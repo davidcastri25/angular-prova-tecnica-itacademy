86d5629d3ebd660a5c1160609254a586374b771a
David Castrillón <david.castri@hotmail.es>
2021-11-03 10:03:15 +0100

    Hecho método para validar (tanto al principio como durante los comandos) que el rover no se sale del campo

--- src/app/prova-tecnica/components/output-text/output-text.component.ts
+++ src/app/prova-tecnica/components/output-text/output-text.component.ts
@@ -11,18 +11,31 @@ import { DataService } from '../../services/data.service';
 export class OutputTextComponent implements OnInit {
   /* PROPERTIES */
   validCommands: boolean = true; //Para validar de nuevo que el array de órdenes tiene valores válidos
+  insideField: boolean = true; //Para validar que estemos dentro del campo
 
   constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    // 0 - Reseteamos propiedades
+    this.validCommands = true;
+    this.insideField = true;
+
     // 1 - Validamos array de órdenes
     this.validCommands = this.dataService.validateAndSetCommands();
 
     //Solo seguiremos si this.validCommands es true
     if (this.validCommands) {
-       // 2 - Configurar los dos objetos (field i rover)
-       this.dataService.setData();
-    } 
+
+      // 2 - Configurar los dos objetos (field i rover)
+      this.dataService.setData();
+
+      //3 - Validamos si hemos aterrizado dentro del campo o ya de entrada estamos fuera
+      this.insideField = this.dataService.roverInsideField();
+
+      //4- Iniciamos algoritmo
+    }
+    
+    //Sacamos el output correspondiente
   }
 
 }
--- src/app/prova-tecnica/services/data.service.ts
+++ src/app/prova-tecnica/services/data.service.ts
@@ -11,14 +11,16 @@ import { field, rover} from '../data/data';
 export class DataService {
   /* Properties */
   formValues: any; //Aquí pongo any porque es el type que me aparece en el elemento que paso como parámetro
-  currentOrientation!: 'N' | 'S' | 'E' | 'W'; //Para controlar en todo momento la orientación del rover  
+  currentOrientation!: 'N' | 'S' | 'E' | 'W'; //Para controlar en todo momento la orientación del rover 
+  insideField: boolean = true; //Propiedad que controlará que no nos salgamos del campo (true: OK, false: nos hemos salido) 
 
   constructor() {}
 
-  /* Método que guardará los datos del formulario en una propiedad del servicio */
+  /* Método que guardará los datos del formulario en propiedad del servicio e inicializará propiedades*/
   passData(formValues: any) {
     this.formValues = formValues;
     this.currentOrientation = formValues.orientation;
+    this.insideField = true;
   }
 
   /* SETTER: Método que asigna los datos del formulario a los objetos JavaScript */
@@ -57,5 +59,36 @@ export class DataService {
 
     //Devolvemos boolean
     return validCommands;
+  }
+
+  /* Método que comprueba que las coordenadas caben en el campo. Devuelve true si OK, devuelve false si nos hemos salido de los límites */
+  roverInsideField(x?: number, y?: number): boolean {
+    //Hay que tener en cuenta que si el usuario nos ha puesto un width de 1, solamente se podrá generar una coordenada X = 0, NO X = 1
+    //Por tanto SIEMPRE x < width i x >= 0 (no pueden ser negativas)
+
+    let currentX, currentY;
+    
+    //Si no pasamos parámetros compararemos la posición inicial
+    if (!x && !y) {
+      currentX = rover.initialPosition.x;
+      currentY = rover.initialPosition.y;
+    }
+    //Si pasamos parámetros estaremos comprobando cada movimiento del rover
+    else {
+      currentX = x; //Parámetro x del método
+      currentY = y; //Parámetro y del método
+    }
+
+    //Hacemos la validación de x
+    if (currentX! >= field.width || currentX! < 0) {
+      this.insideField = false;
+    }
+
+    //Hacemos la validación de y
+    if (currentY! >= field.height || currentY! < 0) {
+      this.insideField = false;
+    }
+
+    return (this.insideField);
   }
 }
